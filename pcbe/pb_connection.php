b7df223094cb50e827c4c499607e80823f5fba4f
krpulasthi <krpulasthi@gmail.com>
2025-02-09 00:48:01 +0530

    database_created and Added

---
+++ pcbe/pb_connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "morgana";
+$username = "suhadamaaru_coop_online_business";
+$password = "Pulasthi@123";
+$dbname = "suhadamaaru_coop_online_business";
+$conn = mysqli_connect($servername, $username, $password);
+$db = mysqli_select_db($conn, $dbname);
+
+//$servername="localhost";    $username="root"; $password=""; $dbname="coop_pb"; $conn=mysqli_connect($servername,$username,$password);  $db=mysqli_select_db($conn,$dbname); 
+
+
+?>
